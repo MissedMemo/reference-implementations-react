0bd0424bcdc4e4f9095ac46e60daa413bd43e983
missedmemo <andrew.lloyd.engineer@gmail.com>
2020-09-28 09:10:24 -0700

    completed merge with master

--- project-templates/devonly-babel-webpack-sass/src/index.js
+++ project-templates/devonly-babel-webpack-sass/src/index.js
@@ -1,13 +1,7 @@
 import React from "react";
 import { render } from "react-dom";
-<<<<<<< HEAD
 import App from "./app";
-
-=======
 
 import "./app.sass";
 
-const App = ({ greeting }) => <>{greeting}</>;
-
->>>>>>> master
 render(<App greeting="Hello, React!" />, document.getElementById("root"));
